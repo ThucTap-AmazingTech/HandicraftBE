8cbf45d35c12a886f7877598f97459794c4007f6
Nguyễn Phương Điền <diennguyenphuong2003@gmail.com>
2024-09-11 10:45:10 +0700

    Create project

---
+++ Handicraft.Contract.Repositories/IUOW/IGenericRepository.cs
@@ -0,0 +1,33 @@
+﻿
+
+using Handicraft.Core.Base;
+
+namespace Handicraft.Contract.Repositories.IUOW
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        // query
+        IQueryable<T> Entities { get; }
+
+        // non async
+        IEnumerable<T> GetAll();
+        T? GetById(object id);
+        void Insert(T obj);
+        void InsertRange(IList<T> obj);
+        void Update(T obj);
+        void Delete(object id);
+        void Save();
+
+        // async
+        Task<IList<T>> GetAllAsync();
+        Task<IList<T>> GetWhereAsync(Func<T, bool> predicate);
+
+       Task<BasePaginatedList<T>> GetPagging(IQueryable<T> query, int index, int pageSize);
+        Task<T?> GetByIdAsync(object id);
+        Task InsertAsync(T obj);
+        Task UpdateAsync(T obj);
+        Task DeleteAsync(object id);
+        Task SaveAsync();
+        Task<bool> ExistsAsync(Func<T, bool> predicate);
+    }
+}
---
+++ Handicraft.Core/Base/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using Hotel.Core.Utils;
+using System.ComponentModel.DataAnnotations;
+namespace Hotel.Core.Base
+{
+    public  class BaseEntity
+    {
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid().ToString("N");
+            CreatedTime = LastUpdatedTime = CoreHelper.SystemTimeNow;
+        }
+
+        [Key]
+        [StringLength(50)]
+        public string Id { get; set; }
+        public string ? InternalCode { get; set; }
+        public string ? CreatedBy { get; set; }
+        public string ? LastUpdatedBy { get; set; }
+        public string ? DeletedBy { get; set; }
+        public DateTimeOffset CreatedTime { get; set; } 
+        public DateTimeOffset LastUpdatedTime { get; set; } 
+        public DateTimeOffset? DeletedTime { get; set; }
+    }
+}
