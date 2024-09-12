70aebb39bcf25b7e60b6a9529ed3ef8f596352c6
deethesaint <vtduong0912@gmail.com>
2024-09-12 09:54:21 +0700

    fix namespace

--- Handicraft.Contract.Repositories/Entities/Carts.cs
+++ Handicraft.Contract.Repositories/Entities/Carts.cs
@@ -6,15 +6,15 @@ using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-namespace Handicraft.Contract.Repositories.Enities
+namespace Handicraft.Contract.Repositories.Entities
 {
     public class Carts
     {
         public required string id { get; set; }
+        //[JsonIgnore]
+        //public virtual User user_id { get; set; }
         [JsonIgnore]
-        public virtual User user_id { get; set; }
-        [JsonIgnore]
-        public virtual ICollection<Product> product_id { get; set; }
+        public virtual ICollection<Products> product_id { get; set; }
         public int amount  { get; set; }
 
     }
--- Handicraft.Contract.Repositories/Entities/Products.cs
+++ Handicraft.Contract.Repositories/Entities/Products.cs
@@ -6,7 +6,7 @@ using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using static System.Net.Mime.MediaTypeNames;
 
-namespace Handicraft.Repositories.Entities
+namespace Handicraft.Contract.Repositories.Entities
 {
     public class Products
     {     
